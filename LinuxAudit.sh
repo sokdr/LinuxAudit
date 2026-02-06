781798feb3f2538e6d2813b52819fd4379d72370
sokdr <20625004+sokdr@users.noreply.github.com>
2026-02-06 13:11:25 +0200

    Update LinuxAudit.sh
    changed path of file. 
    changed case flow

--- LinuxAudit.sh
+++ LinuxAudit.sh
@@ -2,27 +2,23 @@
 #sokdr
 # Clear the screen
 tput clear
-
 # Trap Ctrl+C
 trap ctrl_c INT
-
 # Function to handle Ctrl+C
 function ctrl_c() {
   echo "** You pressed Ctrl+C... Exiting"
   exit 0
 }
-
-
-
+# added path inside script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "Script dir is: $SCRIPT_DIR"
 
 echo "_  _         _     _ _ _  "
 echo "| | (_)_ _ _ ___ __ /_\ _ _ __| (_) |_ "
 echo "| |__| |  \ || \ \ / / _ \ || / _ | | _|"
 echo "|____|_|_||_\_ _/_\_\/_/ \_\_ _\__ _|_|\__|"
 echo
-
 echo "Welcome to security audit of your Linux machine:"
-
 echo
 echo "Script will automatically gather the required info:"
 echo "The checklist can help you in the process of hardening your system:"
@@ -32,21 +28,15 @@ sleep 3
 echo
 
 while true; do
-  read -p "Would you like to save the output? [Y/N]" output
-  case "${output^^}" in
-    Y)
-      read -p "Please enter the path to save the output (e.g., /path_to_save/LinuxAudit.txt):" path
-      echo 
-      echo "File will be saved to $path/LinuxAudit.txt"
-      break
-      ;;
-    N)
-      echo "OK, not saving moving on."
-      break
-      ;;
-    *)
-      echo "Invalid input. Please enter Y or N."
-      ;;
+  echo "Would you like to save the output? " 
+  echo "1) txt"
+  echo "2) terminal output"
+  read -p "Select option 1 or 2 :> " output
+  echo
+
+  case "$output" in
+  1|2) break ;;
+  *) echo "Wrong option" ;;
   esac
 done
 
@@ -246,16 +236,12 @@ Users_and_Groups_Checks
 Networking_Checks
 Services_Checks
 Security_checks
-    
 }
 
-if [[ "${output^^}" == "Y" ]]; then
-  perform_audit > "$path/LinuxAudit.txt"
-else
-  perform_audit
-fi
-echo
-
+case "$output" in
+	1) perform_audit > "$SCRIPT_DIR/LinuxAudit.txt" 2>&1 ;;
+	2) perform_audit ;;
+esac
 echo
 END=$(date +%s)
 DIFF=$(( END - START ))
